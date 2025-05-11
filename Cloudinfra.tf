b109ec8a7eddeaad010a3789bd4e5e999a66e520
vakati.subbu <vakati.subbu@gmail.com>
2025-05-11 19:06:24 +0530

    Second Bucket creation

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "ccitbucket" {
   bucket = "ccitapril"
 }
 
-resource "aws_s3_bucket" "ccitbucket" {
+resource "aws_s3_bucket" "ccitbucket1" {
   bucket = "ccitaprilmay"
 }
 
