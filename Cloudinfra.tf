bbba32951dce938f7a813b458a47ebec5350c47d
Vakatisubbu <94033651+Vakatisubbu@users.noreply.github.com>
2025-05-11 23:22:46 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "eu-west-1"
 }
+resource "aws_s3_bucket" "ccitbucket" {
+  bucket = "ccitaprilMay11"
+}
 
 /*resource "aws_instance" "aws_instanceind" {
 ami = "ami-04e7764922e1e3a57"
@@ -8,11 +11,6 @@ instance_type= "t2.micro"
 subnet_id     = "subnet-0477e85088645156b"
 count =2
 } */
-
-resource "aws_s3_bucket" "ccitbucket" {
-  bucket = "ccitaprilMay11"
-}
-
 
 /*
 resource "aws_s3_bucket" "ccitbucket" {
